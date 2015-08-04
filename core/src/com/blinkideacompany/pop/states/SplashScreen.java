8878a64765426c3867f226d4a864989a92a1a4e2
D Gallagher <dgallagher@Ds-MacBook-Pro.local>
2015-08-04 15:22:06 -0400

    Minor graphical changes.

--- core/src/com/blinkideacompany/pop/states/SplashScreen.java
+++ core/src/com/blinkideacompany/pop/states/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.blinkideacompany.pop.states;
 
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -10,10 +13,16 @@ public class SplashScreen extends State {
     private final int SPLASH_DISPLAY_LENGTH = 2000;
     private Timer t;
     private GameStateManager gsm;
+    private BitmapFont font;
+    private SpriteBatch batch;
 
 
     public SplashScreen() {
         Timer t=new Timer();
+        //specify custom font from interal files
+
+        batch = new SpriteBatch();
+
         gsm=new GameStateManager();
         t.schedule(new TimerTask() {
             @Override
@@ -22,10 +31,13 @@ public class SplashScreen extends State {
             }
         }, SPLASH_DISPLAY_LENGTH);
 
+
     }
 
     @Override
-    public void draw() {
-        Gdx.gl.glClearColor(0,0,1,1);
+    public void draw(ShapeRenderer s) {
+        Label.LabelStyle ls = new Label.LabelStyle();
+
+
     }
 }
